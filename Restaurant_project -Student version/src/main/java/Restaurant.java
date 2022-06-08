ad86ac30293c539463c961684106896b6646e594
dilip0361 <63135883+dilip0361@users.noreply.github.com>
2022-06-08 23:51:59 +0530

    part 3: Solution

--- Restaurant_project -Student version/src/main/java/Restaurant.java
+++ Restaurant_project -Student version/src/main/java/Restaurant.java
@@ -66,6 +66,14 @@ public class Restaurant {
     public String getName() {
         return name;
     }
+        public int getMenuTotalAmount(List<String> itemsSelected){
+        int totalAmount =0;
+        for (String itemName : itemsSelected) {
+            Item item = findItemByName(itemName);
+            totalAmount +=item.getPrice();
+        }
+        return totalAmount;
+    }
 
 
-}
+
